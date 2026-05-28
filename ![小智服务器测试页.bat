2fa695cf09913fc124c839ca6f623fdce6e5e67f
VanillaNahida <nahida1027@126.com>
2026-05-28 20:23:35 +0800

    修复测试页404错误

--- ![小智服务器测试页.bat
+++ ![小智服务器测试页.bat
@@ -4,4 +4,5 @@ chcp 65001 >nul
 title 小智AI服务端测试页Server Test Page
 set "BATCH_DIR=%~dp0"
 set "PYTHON_PATH=%BATCH_DIR%runtime\conda_env\python.exe"
-"%PYTHON_PATH%" ".\scripts\start_test_server.py"
+start "" "http://127.0.0.1:8006/index.html"
+"%PYTHON_PATH%" ".\src\main\digital-human\start.py"
